ad0c7b3897bbae53f45066920e2af5b55c100aa1
24DP4APozn <A240206AP@rvt.lv>
2025-09-29 09:20:44 +0300

    Print the following text

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,7 +1,10 @@
 package rvt;
-
 public class App {
     public static void main(String[] args) {
         // Your code goes here...
+        // Modify the program so that it will print the following text.
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
     }
 }
